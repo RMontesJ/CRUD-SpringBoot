fbcd500149e2b2d4679314b71f31f924c4d1890c
[Rafael Montes] <rafaelmontesjimenez@gmail.com>
2025-02-05 12:04:29 +0100

    Editar y borrar usuario eliminados

--- src/main/java/com/example/crudusuario/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.crudusuario.repository;
-
-import com.example.crudusuario.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Long> {
-}
